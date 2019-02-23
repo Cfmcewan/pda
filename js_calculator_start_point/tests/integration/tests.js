c09de0d5f229c3a508a30bf4e8a0922b4ca75d24
Clare McEwan <cfmcewan@gmail.com>
2019-02-23 15:45:48 +0000

    added calculator testing

---
+++ js_calculator_start_point/tests/integration/tests.js
@@ -0,0 +1,93 @@
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+
+describe('calculator functionality', function() {
+  beforeEach(function() {
+    browser.ignoreSynchronization = true;
+    browser.get('http://localhost:3000');
+  });
+
+  // write integration tests here in the form of "it should do something..."
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
+  it('should concatenate multiple number button clicks', function(){
+    numbers = element(by.css('.number'))
+    element(by.css('#number2')).click();
+    element(by.css('#number2')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('22')
+  })
+
+  it('should chain multiple operations together', function(){
+    operators = element(by.css('.operator'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('3')
+  })
+
+  it('should clear the running total without affecting the calculation', function(){
+    clear = element(by.css('#clear'))
+    element(by.css('#number2')).click();
+    element(by.css('#clear')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0')
+  })
+
+  it('should give an negative number as a result', function(){
+    operators = element(by.css('.operator'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-1')
+  })
+
+  it('should give a decimal number as a result', function(){
+    operators = element(by.css('.operator'))
+    element(by.css('#number9')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4.5')
+
+  })
+
+  it('should manage very large numbers', function(){
+    operators = element(by.css('.operator'))
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2000000000000000500')
+  })
+
+  it('should divide by zero and output not a number', function(){
+    
+  })
+
+});
